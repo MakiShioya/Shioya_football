cbf170bd844b5f288642e7b6bcc289a9833038e0
MakiShioya <makimakisioya@gmail.com>
2026-05-01 04:59:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,12 +13,16 @@ const API_TOKEN = '998d25444615428e83885e7579e83568';
 
 // 自分のサーバー内に「/api/matches」という窓口を作る
 app.get('/api/matches', async (req, res) => {
+    console.log('1. /api/matches へのアクセスがありました');
     try {
+        console.log('2. football-data.org へのデータ要求を開始します');
         const response = await axios.get('https://api.football-data.org/v4/matches', {
             headers: { 'X-Auth-Token': API_TOKEN }
         });
+        console.log('3. football-data.org からのデータ取得に成功しました');
         res.json(response.data);
     } catch (error) {
+        console.log('3. エラーが発生しました: ' + error.message);
         res.status(500).json({ error: 'API取得失敗' });
     }
 });
